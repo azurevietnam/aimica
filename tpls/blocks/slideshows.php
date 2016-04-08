f381b8e0322e690622b206858fc3bc77bce4740e
Azure Vietnam <azurevietnam.net@gmail.com>
2016-04-08 14:55:42 +0700

    Commit_08_04_2016
    Source code template Aimica 08/04/2016

---
+++ tpls/blocks/slideshows.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package   T3 Blank
+ * @copyright Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+?>
+
+<?php if ($this->countModules('slider')) : ?>
+	<div class="slideshows <?php $this->_c('slider') ?>">
+			<jdoc:include type="modules" name="<?php $this->_p('slider') ?>" />
+	</div>
+<?php endif ?>
